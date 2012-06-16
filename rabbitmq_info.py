83202cc3bb9859b8514c405aa93ddbccd209da9f
phrawzty <phrawzty+github@gmail.com>
2012-06-16 16:10:37 +0100

    Released under Apache 2.0 license (issue #4)

--- rabbitmq_info.py
+++ rabbitmq_info.py
@@ -1,11 +1,20 @@
-# rabbitmq-collectd-plugin - rabbitmq_info.py
+# Name: rabbitmq-collectd-plugin - rabbitmq_info.py
+# Author: https://github.com/phrawzty/rabbitmq-collectd-plugin/commits/master
+# Description: This plugin uses Collectd's Python plugin to obtain RabbitMQ metrics.
+# 
+# Copyright 2012 Daniel Maher
 #
-# Author: Daniel Maher (http://www.dark.ca/)
-# Description: This plugin uses collectd's Python plugin to obtain Rabbitmq metrics.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Loosely based on Garret Heaton's "redis-collectd-plugin"
-#   https://github.com/powdahound/redis-collectd-plugin
-
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import collectd
 import subprocess
